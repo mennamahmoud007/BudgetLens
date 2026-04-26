d7d6eebf9d48cb7faf9bb8e57a60cf4b4f580c98
Sara Hany <sara6hany@gmail.com>
2026-04-26 21:54:38 +0300

    Update settings.py

--- budgetlens/settings.py
+++ budgetlens/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'budget_app',
+    'expenses',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# Login URL
+LOGIN_URL = '/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
